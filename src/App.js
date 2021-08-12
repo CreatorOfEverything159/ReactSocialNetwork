4330b793021cd10e2dea75aba8404e029ed28ef8
CreatorOfEverything159 <annoyingfox159@gmail.com>
2021-08-13 01:52:07 +1000

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from "./components/Header/Header";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import Profile from "./components/Profile/Profile";
 
 const App = () => {
     return (
         <div className="App">
             <Header/>
+            <div className="container appWrapper">
+                <NavigationBar/>
+                <Profile/>
+            </div>
         </div>
     );
 }
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Profile.module.css'
+
+const Profile = (props) => {
+    return (
+        <div className={styles.prfile}>
+            profile
+        </div>
+    )
+}
+
+export default Profile;
